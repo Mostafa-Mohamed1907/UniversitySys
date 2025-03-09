5c2dcf736e0c451c410faca6671e22e383cf2ea0
Mostafa M.abdelnaser <mostafa.mohamed851@yahoo.com>
2025-03-10 00:45:12 +0200

    #InstructorController(ShowAllInstructors, Details)

---
+++ UniversitySys/Controllers/InstructorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversitySys.SystemContext;
+
+namespace UniversitySys.Controllers
+{
+    public class InstructorController : Controller
+    {
+        UniversityContext context = new UniversityContext();
+        public IActionResult GetAllInstructors()
+        {
+            var Instruct = context.Instructors.Include("Department").
+                Include("Course").ToList();
+            return View("GetAllInstructors", Instruct);
+        }
+        public IActionResult Details(int id)
+        {
+            var Instruct = context.Instructors.Include("Department").
+                Include("Course").FirstOrDefault(I => I.Id == id);
+            return View("Details", Instruct);
+        }
+        public IActionResult Search(string name)
+        { 
+            var Instruct = context.Instructors.Include("Department").
+                Include("Course").FirstOrDefault(I => I.Name == name);
+            if (Instruct != null)
+            {
+               return RedirectToAction ("Details", Instruct);
+            }
+            ViewBag.Message = "Instructor not found";
+            return View("Search", null);
+        }
+
+
+    }
+}
