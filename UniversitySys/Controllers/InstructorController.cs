b799731a05c36c083af9db4432eb1049fdc71065
Mostafa M.abdelnaser <mostafa.mohamed851@yahoo.com>
2025-03-14 01:13:04 +0200

    #AddNewInstructor

--- UniversitySys/Controllers/InstructorController.cs
+++ UniversitySys/Controllers/InstructorController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using UniversitySys.Models;
 using UniversitySys.SystemContext;
+using UniversitySys.ViewModel;
 
 namespace UniversitySys.Controllers
 {
@@ -19,6 +21,42 @@ namespace UniversitySys.Controllers
                 Include("Course").FirstOrDefault(I => I.Id == id);
             return View("Details", Instruct);
         }
+        public IActionResult Add()
+        {
+            var InstructorDeptCrsViewModel = new InstructorDeptCrsViewModel
+            {
+                DepartmentList = context.Departments.ToList(),
+                CourseList = context.Courses.ToList()
+            };
+            return View("Add", InstructorDeptCrsViewModel);
+        }
+        public IActionResult SaveAdd(InstructorDeptCrsViewModel instructorDeptCrsViewModel)
+        {
+            //InstructorDeptCrsViewModel instructorDeptCrsViewModel
+            //    = new InstructorDeptCrsViewModel();
+            Instructor instructor = new Instructor();
+            instructorDeptCrsViewModel.DepartmentList = context.Departments.ToList();
+            instructorDeptCrsViewModel.CourseList = context.Courses.ToList();
+
+            instructor.Id = instructorDeptCrsViewModel.Id;
+            instructor.Name = instructorDeptCrsViewModel.Name;
+            instructor.Image = instructorDeptCrsViewModel.Image;
+            instructor.Salary = instructorDeptCrsViewModel.Salary;
+            instructor.Address = instructorDeptCrsViewModel.Address;
+            instructor.DepartmentId = instructorDeptCrsViewModel.DepartmentId;
+            instructor.CourseId = instructorDeptCrsViewModel.CourseId;
+            if (instructor.Name != null)
+            {
+                context.Instructors.Add(instructor);
+                context.SaveChanges();
+                return RedirectToAction("GetAllInstructors");
+            }
+
+            //DEPTList = instructorDeptCrsViewModel.DepartmentList;
+            //CRSList = instructorDeptCrsViewModel.CourseList;
+            return View("Add", instructorDeptCrsViewModel);
+        }
+
         public IActionResult Search(string name)
         { 
             var Instruct = context.Instructors.Include("Department").
---
+++ UniversitySys/ViewModel/InstructorDeptCrsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using UniversitySys.Models;
+
+namespace UniversitySys.ViewModel
+{
+    public class InstructorDeptCrsViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public double Salary { get; set; }
+        public string? Address { get; set; }
+        public int DepartmentId { get; set; }
+        public int CourseId { get; set; }
+        public List<Department>? DepartmentList { get; set; }
+        public List<Course>? CourseList { get; set; }
+    }
+}
