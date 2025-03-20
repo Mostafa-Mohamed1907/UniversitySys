30e0be19225b82994dde07830c721f5698c8f4c7
Mostafa M.abdelnaser <mostafa.mohamed851@yahoo.com>
2025-03-20 06:45:09 +0200

    #TraineeCrsResultViewModel #Extracting_The_Trainee_Grade_InShowREsult

---
+++ UniversitySys/Controllers/TraineeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversitySys.Models;
+using UniversitySys.SystemContext;
+using UniversitySys.ViewModel;
+
+namespace UniversitySys.Controllers
+{
+    public class TraineeController : Controller
+    {
+        UniversityContext context = new UniversityContext();
+        [HttpGet]
+        public IActionResult ShowREsult(int id, int crsid)
+        {
+            var trainee = context.CrsResults
+                .Include(c => c.Trainee)
+                .Include(c => c.Course)
+                .FirstOrDefault(c => c.TraineeId == id && c.CourseId == crsid);
+
+            if (trainee == null)
+            {
+                ModelState.AddModelError("", "No data found for the provided Trainee ID and Course ID.");
+                return View();
+            }
+
+            TraineeCrsResultViewModel traCrsResultViewModel = new TraineeCrsResultViewModel
+            {
+                TraineeId = trainee.TraineeId,
+                CourseId = trainee.CourseId,
+                TraineeName = trainee.Trainee?.Name ?? "Unknown",
+                CourseName = trainee.Course?.Name ?? "Unknown",
+                TraineeDegree = trainee.Degree
+            };
+
+            if (trainee.Degree >= trainee.Course.MinDegree)
+            {
+                traCrsResultViewModel.Status = "Pass";
+                traCrsResultViewModel.Color = "green";
+            }
+            else
+            {
+                traCrsResultViewModel.Status = "Fail";
+                traCrsResultViewModel.Color = "red";
+            }
+
+            return View("ShowREsult", traCrsResultViewModel);
+        }
+    }
+}
---
+++ UniversitySys/ViewModel/TraineeCrsResultViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace UniversitySys.ViewModel
+{
+    public class TraineeCrsResultViewModel
+    {
+        public int Id { get; set; }
+        public string TraineeName { get; set; }
+        public string CourseName { get; set; }
+        public int TraineeDegree { get; set; }
+        public string Color { get; set; }
+        public string Status { get; set; }
+        public int TraineeId { get; set; }
+        public int CourseId { get; set; }
+
+
+    }
+}
